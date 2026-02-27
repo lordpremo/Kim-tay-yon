29505776fe48cad305ef71cfd80da1fde0d8ddb0
LORD PREMO <cypherx599@gmail.com>
2026-02-27 09:44:42 +0300

    Create Mission.cs

---
+++ Assets/Scripts/Missions/Mission.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Mission
+{
+    public string id;
+    public string title;
+    public string description;
+    public bool isCompleted;
+}
