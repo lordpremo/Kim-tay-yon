99822daac8776cf6667e1b2a16d2d827771e94b4
LORD PREMO <cypherx599@gmail.com>
2026-02-27 10:28:16 +0300

    Create SceneLoader.cs

---
+++ Assets/Scripts/Systems/SceneLoader.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
