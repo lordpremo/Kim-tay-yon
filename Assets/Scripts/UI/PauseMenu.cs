aa11c42816c78eaf3e61acc06ad650cdbd3d8f29
LORD PREMO <cypherx599@gmail.com>
2026-02-27 10:25:33 +0300

    Create PauseMenu.cs

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (pausePanel != null)
+            pausePanel.SetActive(isPaused);
+
+        Time.timeScale = isPaused ? 0f : 1f;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneLoader.LoadScene("MainMenu");
+    }
+}
